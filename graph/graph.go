1b20a0030b561192938bf16d6c9446c953ec4070
Vincent Bloemen <v.bloemen@utwente.nl>
2016-09-26 13:49:29 +0200

    Add explicit graph structure

---
+++ graph/graph.go
@@ -0,0 +1,9 @@
+// Package graph provides an interface for specifying graphs and contains
+// implementations of several example graphs.
+package graph
+
+// Interface object for the graph type.
+type Graph interface{
+	Init() int
+	Successors(state int) []int
+}
